82cde30eb578bdcdf6d13872809ab45f7fed6bcc
Gus Mueller <gus@asecular.com>
2025-10-28 09:20:24 -0400

    Change URL for weather data retrieval

--- config.c
+++ config.c
@@ -3,5 +3,5 @@ const char* wifi_ssid = "your_ssid"; //mine was Moxee Hotspot83_2.4G
 const char* wifi_password = "super_secret_password";
 const char* storage_password = "other_password"; //to ensure someone doesn't store bogus data on your server. should match value in the storage_password column in you user table for your user
 
-const char* url_get = "/weather/data.php";
+const char* url_get = "/weather/server.php";
 const char* host_get = "hmmm.com";
